ac455c634171a081a775fe491e765c15f1aa865b
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-12-07 16:21:05 +0800

    添加插件主文件，方便以标准WordPress插件方式安装

---
+++ wenprise-debug.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:        Wenprise Debug
+Plugin URI:         https://www.wpzhiku.com/
+Description:        Laravel dd() & dda() function for WordPress
+Version:            1.0.0
+Author:             WordPress 智库
+Author URI:         https://www.wpzhiku.com/
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
+
+require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
